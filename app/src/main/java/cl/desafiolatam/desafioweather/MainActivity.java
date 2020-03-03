6291ed1a8bef17c0e4a3bf87417d1e698588c2cc
Kevin <kevin.floba@gmail.com>
2020-03-03 13:08:45 -0300

    Second Commit

--- app/src/main/java/cl/desafiolatam/desafioweather/MainActivity.java
+++ app/src/main/java/cl/desafiolatam/desafioweather/MainActivity.java
@@ -1,14 +1,19 @@
 package cl.desafiolatam.desafioweather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 
 import android.os.Bundle;
+
+import cl.desafiolatam.desafioweather.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding viewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        viewDataBinding.textView.setText("Welcome again!");
     }
 }
